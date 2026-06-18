4b725735ac1595208fe8456738c31c8aea79f6a8
vinitaran <52027320+vinitaran@users.noreply.github.com>
2026-06-18 09:15:45 +0200

    Add: is-anagram - submission-1

---
+++ Data Structures & Algorithms/is-anagram/submission-1.js
@@ -0,0 +1,25 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @param {string} t
+     * @return {boolean}
+     */
+    isAnagram(s, t) {
+
+        const arr1 = [...s]
+        const arr2 = [...t]
+        arr1.sort()
+        arr2.sort()
+        if(arr1.length === arr2.length){
+            for(let i = 0; i < arr1.length; i++){
+                if(arr1[i] !== arr2[i]){
+                    return false
+                }
+            }
+            return true
+        }else{
+            return false
+        }
+        
+    }
+}
