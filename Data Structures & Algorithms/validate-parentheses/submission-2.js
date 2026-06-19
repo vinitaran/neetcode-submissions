b02e73205e7fbf9b50c94f9058175a72b4ee361e
vinitaran <52027320+vinitaran@users.noreply.github.com>
2026-06-19 10:01:46 +0200

    Add: validate-parentheses - submission-2

---
+++ Data Structures & Algorithms/validate-parentheses/submission-2.js
@@ -0,0 +1,24 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @return {boolean}
+     */
+    isValid(s) {
+        const stash = []
+        const hash = {']': '[', ')' : '(', '}' : '{'}
+
+        for(let i = 0; i < s.length; i++){
+            if(hash[s[i]]){
+                if(stash.length && hash[s[i]] === stash[stash.length-1]){
+                    stash.pop()
+                }else{
+                    return false
+                }
+                
+            }else{
+                stash.push(s[i])
+            }
+        }
+        return stash.length === 0;
+    }
+}
