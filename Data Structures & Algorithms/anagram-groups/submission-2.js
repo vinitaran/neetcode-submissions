fc98a4fb8141f4a36b0c2f6d9223a47dbfd7789b
vinitaran <52027320+vinitaran@users.noreply.github.com>
2026-07-06 09:00:27 +0200

    Add: anagram-groups - submission-2

---
+++ Data Structures & Algorithms/anagram-groups/submission-2.js
@@ -0,0 +1,21 @@
+class Solution {
+    /**
+     * @param {string[]} strs
+     * @return {string[][]}
+     */
+    groupAnagrams(strs) {
+        let res = {}
+        for(let s of strs){
+            let count = new Array(26).fill(0)
+            for(let c of s){
+                count[c.charCodeAt(0) - 'a'.charCodeAt(0)]++
+            }
+            let key = count
+            if(!res[key]){
+                res[key]=[]
+            }
+            res[key].push(s)
+        }
+        return Object.values(res)
+    }
+}
